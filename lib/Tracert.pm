95dd84877de84331291ce24abebb49ffb45e9971
Gabor Szabo <gabor@szabgab.com>
2014-11-05 10:20:44 +0200

    set gw ids starting from 1 and check if there are duplicate ids

--- lib/Tracert.pm
+++ lib/Tracert.pm
@@ -11,10 +11,17 @@ sub check_sites {
 	my ($self) = @_;
 
 	my $data = Tracert::DB->new( root => $self->root )->load_data();
+	
+	my %ids;
 
 	#die Dumper $data;
 
 	foreach my $gw ( @{ $data->{gateways} } ) {
+		if ($ids{ $gw->{id} }) {
+			warn "Duplicate ID '$gw->{id}'\n";
+		}
+		$ids{ $gw->{id} } = $gw;
+
 		if ( $gw->{status} eq 'SHOW' ) {
 			my $url = "$gw->{url}$gw->{path}";
 			say
